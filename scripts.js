2731d14a85c937962cc7c94e1b377ac429fcb6f4
Chidozie Matthew <Kellyvic111@gmail.com>
2026-06-29 12:53:11 +0100

    correction of currency

--- scripts.js
+++ scripts.js
@@ -7,11 +7,11 @@ const rateText = document.querySelector('.rate');
 
 
 const rates = {
-    USD: { EUR: 0.86, NGN: 1600, POUNDS: 0.65, YUAN: 1400, USD: 1 },
-    EUR: { USD: 1.16, NGN: 1850, POUNDS: 0.82, YUAN: 1660, EUR: 1 },
-    POUNDS: { EUR: 1.85, USD: 2.45, NGN: 2200, YUAN: 1800, POUNDS: 1 },
-    YUAN: { EUR: 0.45, USD: 0.56, POUNDS: 0.12, NGN: 0.70, YUAN: 1 },
-    NGN: { EUR: 0.40, USD: 0.50, POUNDS: 0.10, YUAN: 0.60, NGN: 1 },
+    USD: { EUR: 0.88, NGN: 1381.20, POUNDS: 0.76, YUAN: 6.80, USD: 1 },
+    EUR: { USD: 1.14, NGN: 1575.30, POUNDS: 0.86, YUAN: 7.75, EUR: 1 },
+    POUNDS: { EUR: 1.16, USD: 1.32, NGN: 1.827, YUAN: 8.99, POUNDS: 1 },
+    YUAN: { EUR: 0.13, USD: 0.15, POUNDS: 0.11, NGN: 203.28, YUAN: 1 },
+    NGN: { EUR: 0.00063, USD: 0.00072, POUNDS: 0.00055, YUAN: 0.0049, NGN: 1 },
 };
 
 function currencyConvert() {
